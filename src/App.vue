f41179675560d56d0c010313b5a25af641e6ab11
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-10-06 17:00:33 +0300

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,31 @@
 
 <template>
   <div class="container">
-    <div class="left_side">left side</div>
-    <div class="middle">middle</div>
-    <div class="right_side">right side</div>
+    <div class="sidebar">left side</div>
+    <div class="feed">middle</div>
+    <div class="widgets">right side</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .container {
+  max-width: 1300px;
+  min-height: 100vh;
+  width: 100%;
   display: flex;
   justify-content: center;
+  margin: 0 auto;
 }
-.middle {
-  width: 578px;
-  border: 1px solid black;
+.sidebar {
+  width: 20%;
+}
+.feed {
+  border-left: 0.5px solid var(--border-main);
+  border-right: 1px solid var(--border-main);
+  width: 50%;
+}
+.widgets {
+  width: 30%;
+  text-align: right; //temp
 }
 </style>
