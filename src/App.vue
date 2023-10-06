30e60ccee835caa42229f233a94b8a8724d42cc7
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-10-06 16:37:54 +0300

    setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="left_side">left side</div>
+    <div class="middle">middle</div>
+    <div class="right_side">right side</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+.middle {
+  width: 578px;
+  border: 1px solid black;
+}
+</style>
