187ed26df31783cdd76b2a444af4bb447402cb44
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-10-10 20:42:10 +0300

    remodeled createPost sections/ added forms, username, handle inputs

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
+import Post from "./Post.vue";
 import { ref } from "vue";
-const text = ref("");
+
+const text = ref<string>("");
+const username = ref<string>("");
+const handle = ref<string>("");
+const allowPost = ref<boolean>(false);
+
+function submitForm(event: Event) {
+  event.preventDefault();
+  text.value = "";
+  username.value = "";
+  handle.value = "";
+  allowPost.value = false;
+}
+function handleAllowPost() {
+  if (
+    text.value.length !== 0 &&
+    username.value.length !== 0 &&
+    handle.value.length !== 0
+  ) {
+    allowPost.value = true;
+  } else {
+    allowPost.value = false;
+  }
+}
 </script>
 
 <template>
@@ -12,183 +36,59 @@ const text = ref("");
         <div class="">Following</div>
       </div>
     </header>
-    <div class="createPost">
-      <div class="createPost__top">
-        <figure>
-          <img
-            src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
-            alt=""
-          />
-        </figure>
-        <textarea
-          type="text"
-          placeholder="What is happening?!"
-          v-model="text"
-        />
-      </div>
-      <div class="createPost__bottom">
-        <div class="createPost__options">
-          <font-awesome-icon icon="fa-regular fa-image" />
-          <font-awesome-icon icon="fa-regular fa-square" />
-          <font-awesome-icon icon="fa-solid fa-pause" />
-          <font-awesome-icon icon="fa-regular fa-face-smile" />
-          <font-awesome-icon icon="fa-regular fa-calendar" />
-          <font-awesome-icon icon="fa-solid fa-location-dot" />
+    <form type="submit" @submit="submitForm">
+      <div class="createPost">
+        <div class="createPost__top">
+          <figure>
+            <img
+              src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+              alt=""
+            />
+          </figure>
+          <div class="inputs">
+            <div class="usernames">
+              <input
+                type="text"
+                placeholder="Username"
+                maxlength="15"
+                @change="handleAllowPost"
+                v-model="username"
+                required
+              />
+              <input
+                type="text"
+                placeholder="Handle"
+                maxlength="15"
+                @change="handleAllowPost"
+                required
+                v-model="handle"
+              />
+            </div>
+            <textarea
+              type="text"
+              placeholder="What is happening?!"
+              maxlength="200"
+              v-model="text"
+              @change="handleAllowPost"
+              required
+            />
+          </div>
         </div>
-        <button :disabled="true">Post</button>
+        <div class="createPost__bottom">
+          <div class="createPost__options">
+            <font-awesome-icon icon="fa-regular fa-image" />
+            <font-awesome-icon icon="fa-regular fa-square" />
+            <font-awesome-icon icon="fa-solid fa-pause" />
+            <font-awesome-icon icon="fa-regular fa-face-smile" />
+            <font-awesome-icon icon="fa-regular fa-calendar" />
+            <font-awesome-icon icon="fa-solid fa-location-dot" />
+          </div>
+          <button type="submit" :disabled="!allowPost">Post</button>
+        </div>
       </div>
-    </div>
+    </form>
     <main>
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
-      feed
-      <br />
+      <Post />
     </main>
   </section>
 </template>
@@ -205,40 +105,40 @@ header {
   position: sticky;
   top: 0;
   backdrop-filter: blur(12px);
-}
 
-.header__title {
-  font-size: 20px;
-  font-weight: 900;
-  height: 50%;
-  padding: 0 16px;
-  display: flex;
-  align-items: center;
-}
-
-.header__feedOptions {
-  font-size: 16px;
-  display: flex;
-  height: 50%;
-  div {
-    transition: all 300ms ease;
-    width: 50%;
+  .header__title {
+    font-size: 20px;
+    font-weight: 900;
+    height: 50%;
+    padding: 0 16px;
     display: flex;
-    justify-content: center;
     align-items: center;
-    color: gray;
-    cursor: pointer;
-    &:hover {
-      background-color: rgba(#181919, 0.5);
-    }
   }
-  .active {
-    text-decoration: underline;
-    text-decoration-color: var(--brandBlue);
-    text-decoration-thickness: 4px;
-    text-underline-offset: 90%;
-    font-weight: 700;
-    color: var(--text-main);
+
+  .header__feedOptions {
+    font-size: 16px;
+    display: flex;
+    height: 50%;
+    div {
+      transition: all 300ms ease;
+      width: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: gray;
+      cursor: pointer;
+      &:hover {
+        background-color: rgba(#181919, 0.5);
+      }
+    }
+    .active {
+      text-decoration: underline;
+      text-decoration-color: var(--brandBlue);
+      text-decoration-thickness: 4px;
+      text-underline-offset: 90%;
+      font-weight: 700;
+      color: var(--text-main);
+    }
   }
 }
 
@@ -249,25 +149,43 @@ header {
   border-bottom: 1px solid var(--border-main);
   padding: 16px;
   padding-bottom: 0;
-  figure {
-    width: 40px;
-    height: 40px;
-    border-radius: 100%;
-    overflow: hidden;
-    margin-right: 16px;
-  }
   .createPost__top {
     display: flex;
+    width: 100%;
+    figure {
+      width: 40px;
+      height: 40px;
+      border-radius: 100%;
+      overflow: hidden;
+      margin-right: 16px;
+    }
+    .inputs {
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      .usernames {
+        display: flex;
+        font-size: 12px;
+      }
+    }
+
+    input,
     textarea {
       color: var(--text-main);
       flex: 1;
       background: none;
       outline: none;
-      font-size: 20px;
+      /* font-size: 20px; */
       resize: none;
       &::placeholder {
         color: gray;
       }
+    }
+    textarea {
+      margin-top: 8px;
+      font-size: 20px;
+      /* width: 100%; */
+      flex: 1;
     }
   }
   .createPost__bottom {
@@ -291,7 +209,6 @@ header {
         aspect-ratio: 1/1;
         cursor: pointer;
         &:hover {
-          /* background-color: var(--background-secondary); */
           background-color: rgba(#1d9bf0, 0.1);
         }
       }
@@ -303,10 +220,15 @@ header {
       color: var(--text-main);
       font-weight: 700;
       transition: all 200ms ease;
+      cursor: pointer;
       &:hover {
         filter: brightness(0.85);
       }
+      &:not(:disabled):active {
+        transform: scale(0.9);
+      }
       &:disabled {
+        cursor: default;
         filter: brightness(0.6);
       }
     }
